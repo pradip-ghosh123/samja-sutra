997fc5f001534b4a78b4742066419041bcd9a0b0
shibnath96 <32192820+shibnath96@users.noreply.github.com>
2019-04-14 22:42:15 +0530

    [Forgot password]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { LoginPortalComponent } from './login/login-portal/login-portal.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,10 @@ const routes: Routes = [
   {
     path: 'app/dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
   },
   {
     path: '**',
